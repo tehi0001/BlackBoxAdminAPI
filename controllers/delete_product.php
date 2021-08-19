86803d5e305d1fc95ecf5b09f390c606c3eb4ea0
Timothy Ehimen <tehi0001@student.monash.edu>
2021-08-19 23:17:55 +0200

    Fixed delete product

--- controllers/delete_product.php
+++ controllers/delete_product.php
@@ -1,1 +1,16 @@
 <?php
+
+$user = Utils::get_user_from_session($response);
+
+$post = $request->getBody();
+
+$db = Utils::get_db_object();
+
+try {
+    $db->query("DELETE FROM products WHERE id=?", "i", array($post['id']));
+    $response->json(array(
+        "success" => true
+    ));
+} catch (Exception $e) {
+    $response->status(500)->send($e->getMessage());
+}
